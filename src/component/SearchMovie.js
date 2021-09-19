cef8926270f0a76b8dab6a7f7d56de27a11edacd
naima-shk <naimashaikh550@gmail.com>
2021-09-19 23:13:22 +0500

    'implement-movie-app-using-react'

--- src/component/SearchMovie.js
+++ src/component/SearchMovie.js
@@ -31,7 +31,14 @@ function SearchMovie() {
         <div className='card-list'>
             {movies.map(movie => (
                 <div className='card'>
+                   
                     <img className='card--image'  src={`https://www.imdb.com/title/tt0107290/mediaviewer/rm3913805824/${movie.poster_path}`} alt={movie.title + 'poster'}/>
+                 <div className='card--content'>
+                     <h3 className='card--title'>{movie.title}</h3>
+                     <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                     <p><small>RATING: {movie.vote_average}</small></p>
+                     <p className='card--desc'>{movie.overview}</p>
+                 </div>
                 </div>
 
             ))}
